32b701e23d99e19014ecf5ba1b49b6276de57014
Tom Gregor <tgregor@ivsoftware.com>
2023-03-27 10:18:50 -0600

    Add project files.

---
+++ speech-recognition-listener-4-8/SpeechForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Speech.Recognition;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace speech_recognition_listener_4_8
+{
+    public partial class SpeechForm : Form
+    {
+        public SpeechForm()
+        {
+            InitializeComponent();
+
+            // https://learn.microsoft.com/en-us/dotnet/api/system.speech.recognition.speechrecognizer.loadgrammar?view=netframework-4.8.1
+            SRE = new SpeechRecognitionEngine();
+            SRE.SpeechRecognized += onSpeechRecognized;
+            Grammar testGrammar =
+              new Grammar(new GrammarBuilder("testing"));
+            testGrammar.Name = "Test Grammar";
+            SRE.LoadGrammar(testGrammar);
+            SRE.SetInputToDefaultAudioDevice();
+            SRE.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        protected async override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Application.Exit();
+        }
+
+        private void onSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            BeginInvoke(new Action(() => textBox1.Text = e.Result.Text));
+        }
+        SpeechRecognitionEngine SRE { get; }
+    }
+}
---
+++ speech-recognition-listener-4-8/SpeechForm.designer.cs
@@ -0,0 +1,76 @@
+﻿namespace speech_recognition_listener_4_8
+{
+    partial class SpeechForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMic = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelMic
+            // 
+            this.labelMic.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.labelMic.Image = global::speech_recognition_listener_4_8.Properties.Resources.mic;
+            this.labelMic.Location = new System.Drawing.Point(33, 57);
+            this.labelMic.Name = "labelMic";
+            this.labelMic.Size = new System.Drawing.Size(81, 83);
+            this.labelMic.TabIndex = 0;
+            this.labelMic.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox1.Location = new System.Drawing.Point(134, 32);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 182);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Listening...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 240);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelMic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelMic;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
